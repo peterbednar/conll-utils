5bee8c27c50af69e84e3de40c7e88cfacba9f49f
Peter Bednar <naivebayes@gmail.com>
2019-09-04 23:14:09 +0200

    Moved to top directory

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     name="CoNLLUtils",
     packages=["conllutils"],
     version=VERSION,
-    long_description=open(os.path.join(os.path.dirname(__file__), "../README.md")).read(),
-    long_description_content_type="text/markdown",
+    license="MIT",
+    description="Utility classes and functions for parsing and indexing of files in CoNLL-U format.",
     author=u"Peter Bednár",
     author_email="peter.bednar@tuke.sk",
     url="https://github.com/peterbednar/conllutils",
+    install_requires=["numpy"]
 )
