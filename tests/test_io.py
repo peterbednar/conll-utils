5c9ec85cfbe972652ea47cc68b8fd7d82ce40de3
Peter Bednar <naivebayes@gmail.com>
2020-04-26 21:35:15 +0200

    Added tests for io module

--- tests/test_io.py
+++ tests/test_io.py
@@ -7,6 +7,10 @@ from conllutils.pipeline import pipe
 def _data_filename(name):
     return os.path.join(os.path.dirname(__file__), name)
 
+def _read_file(name):
+    with open(name, "rt", encoding="utf-8") as f:
+        return f.read()
+
 @pytest.fixture
 def data1():
     return _data_filename("data1.conllu")
@@ -23,7 +27,36 @@ def data4():
 def data5():
     return _data_filename("data5.conllu")
 
-def test_hdf5(data3, tmp_path):
+def test_read_write_file():
+    with pytest.raises(ValueError):
+        pipe().read_file('temp', 'unknown').collect()
+
+def test_collu(data2, tmp_path):
+    sentences = pipe().read_conllu(data2).collect()
+    filename = tmp_path / 'data.conllu'
+
+    pipe(sentences).write_file(filename, 'conllu')
+    assert pipe().read_file(filename, 'conllu').collect() == sentences
+
+def test_txt(tmp_path):
+    lines1 = ["text"] * 10
+    filename = tmp_path / 'data.txt'
+    pipe(lines1).write_file(filename, 'txt')
+    lines2 = pipe().read_file(filename, 'txt').collect()
+
+    assert lines2 == [l + '\n' for l in lines1]
+
+def test_hdf5(data2, data3, tmp_path):
+    index = pipe().read_conllu(data2).create_index()
+    instances1 = pipe().read_conllu(data2).to_instance(index).collect()
+
+    filename = tmp_path / 'data.hdf5'
+    pipe(instances1).write_file(filename, 'hdf5')
+    instances2 = pipe().read_file(filename, 'hdf5').collect()
+
+    for ins1, ins2 in zip(instances1, instances2):
+        equal_instance(ins1, ins2)
+
     index = pipe().read_conllu(data3).create_index()
     instances1 = pipe().read_conllu(data3).to_instance(index).collect()
 
